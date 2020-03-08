67d6a83778119015dba4367b07cb856f29a8107d
Jose E Reyes <jreyes8631@gmail.com>
2020-03-08 20:13:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,8 +15,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  letter = ["a", "b", "c", "d"]
-  letter.unshift("f")
+  letter.unshift
   
   
 end
